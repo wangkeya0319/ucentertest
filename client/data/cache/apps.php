4bf2d0a90e6db5694be7c3e283b293670eac2fdd
wangkeya0319 <usawangkeya0319@gmail.com>
2015-07-13 10:53:27 -0400

    good push

---
+++ client/data/cache/apps.php
@@ -0,0 +1,45 @@
+<?php
+$_CACHE['apps'] = array (
+  5 => 
+  array (
+    'appid' => '5',
+    'type' => 'OTHER',
+    'name' => 'wordpress',
+    'url' => 'http://nycdatascience.com/wp-content/plugins/ucenter-integration',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => false,
+    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?>
+<root>
+	<item id="template"><![CDATA[]]></item>
+</root>',
+    'allowips' => '',
+  ),
+  3 => 
+  array (
+    'appid' => '3',
+    'type' => 'OTHER',
+    'name' => 'eduwind',
+    'url' => 'http://edu.nycdatascience.com/partner/ucenter',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => false,
+    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?>
+<root>
+	<item id="template"><![CDATA[]]></item>
+</root>',
+    'allowips' => '',
+  ),
+);
+
+?>
---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+define('UC_CONNECT', 'mysql');
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'ucenter');
+define('UC_DBPW', '123456nyc');
+define('UC_DBNAME', 'ucenter');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ucenter`.uc_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', 'f089KOdFyvjA0uyvM6V24FeWcRAGBhER+kE8x88');
+define('UC_API', 'http://ucenter.nycdatascience.com');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '');
+define('UC_APPID', '5');
+define('UC_PPP', '20');
+?>
